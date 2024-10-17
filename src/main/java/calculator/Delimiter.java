b0f2a462e53531fdb7b7ce26ed3fb6930fbdb09e
woody6624 <woody6624@gmail.com>
2024-10-17 14:40:28 +0900

    fix(Delimiter): Delimiter클래스 내부 검증 로직 강화 및 에러 처리 분기점 추가

--- src/main/java/calculator/Delimiter.java
+++ src/main/java/calculator/Delimiter.java
@@ -4,16 +4,32 @@ public class Delimiter {
     private String default_delimiters = ",|:";
 
     public String addDelimiter(String inputData) {
-        StringBuffer delimiters = new StringBuffer(default_delimiters);
+        StringBuilder delimiters = new StringBuilder(default_delimiters);
+
         if (inputData.startsWith("//")) {
-            char customDelimiter = inputData.charAt(2);
-            // 숫자로 된 구분자 혹은 구분자가 아예 없을떄 에러
-            if (Character.isDigit(customDelimiter) || customDelimiter == '\\') {
-                throw new IllegalArgumentException("잘못된 구분자 타입입니다.");
+            int newLineIndex = inputData.indexOf("\n");
+
+            // '\n'이 없다면 에러 -> 커스텀 구분자 형식에 어긋남
+            if (newLineIndex == -1) {
+                throw new IllegalArgumentException("입력 에러 //뒤에 오는 구분자 형식에 어긋납니다.");
             }
+
+            char customDelimiter = inputData.charAt(2);
+            // 빈 구분자에 대한 에러 처리
+            if (customDelimiter == '\\') {
+                throw new IllegalArgumentException("잘못된 입력입니다.");
+            }
+            // 구분자가 숫자인지 체크
+            if (Character.isDigit(customDelimiter)) {
+                throw new IllegalArgumentException("숫자는 구분자가 될 수 없습니다.");
+            }
+            // 커스텀 구분자를 추가
             delimiters.append("|").append(customDelimiter);
+            // 구분자를 제외한 나머지 문자열 반환
+            return inputData.substring(5);
+        } else {
+            // 변경사항이 없으므로 받은 그대로 리턴
+            return inputData;
         }
-        // 추가한 구분자를 포함한 구분자에 대한 정보를 리턴
-        return delimiters.toString();
     }
 }
