e36651591023e1aa505f6552b5dac9feb9913196
woody6624 <woody6624@gmail.com>
2024-10-17 14:55:48 +0900

    feat(StringCaculator): 파싱을 위한 InputParser와 Validator를 사용하고 검증되고 파싱된 데이터를 더한 결과값을 반환하는 StringCalculator 클래스 생성

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,31 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StringCalculator {
+    private InputParser inputParser;
+    private Validator validator;
+
+    public StringCalculator() {
+        this.inputParser = new InputParser();
+        this.validator = new Validator();
+    }
+
+    public int add(String inputData) {
+        // 입력 데이터를 파싱하여 유효값만 리스트에 추가
+        String parsedInput = inputParser.parse(inputData);
+        List<String> numbers = Arrays.stream(inputData.split(parsedInput)).toList();
+        int sum = 0;
+        for (String number : numbers) {
+            // 
+            if (!validator.checkPositiveInt(number)) {
+                throw new IllegalArgumentException("데이터 검증 과정에서 오류");
+            }
+            // 문자열을 정수로 파싱하여 합산
+            sum += Integer.parseInt(number);
+        }
+
+        return sum;
+    }
+}
